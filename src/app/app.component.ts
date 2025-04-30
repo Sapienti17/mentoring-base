4ad5136ef84d24e8ee8c9e66aee07fabd81214e8
Sapienti <sapienti17@gmail.com>
2025-04-30 17:13:26 +0300

    fixed bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core'
+import {RouterOutlet} from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'mentoring-first-project';
+  title = 'mentoring-first-project'
+
+  readonly mainTitle: string = 'Главная'
+  readonly aboutTitle: string = 'О компании'
+  readonly catalogTitle: string = 'Каталог'
+
+  readonly subTitleListItem1: string = 'Каталог'
+  readonly subTitleListItem2: string = 'Строительные материалы'
+  readonly subTitleListItem3: string = 'Инструменты'
+  readonly subTitleListItem4: string = 'Электрика'
+  readonly subTitleListItem5: string = 'Интерьер и одежда'
+
 }
