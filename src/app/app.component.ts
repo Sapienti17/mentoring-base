18036f981fd5215129e7227ddf5c65f3326d6630
Sapienti <sapienti17@gmail.com>
2025-05-04 22:36:59 +0300

    added new features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,44 @@
 import {Component} from '@angular/core'
 import {RouterOutlet} from '@angular/router'
+import {NgFor, NgIf, NgOptimizedImage} from "@angular/common";
+
+
+const returnMenuItem: (item: string) => string = (item: string): string => {
+  return item
+}
+
+const menuAboutCompany: string = returnMenuItem("О компании");
+
+const newPages: number[] = [5, 4, 3, 2, 1]
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgIf, NgOptimizedImage, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'mentoring-first-project'
+  title: string = 'mentoring-first-project'
 
   readonly mainTitle: string = 'Главная'
-  readonly aboutTitle: string = 'О компании'
+  readonly aboutCompany: string = menuAboutCompany
   readonly catalogTitle: string = 'Каталог'
 
-  readonly subTitleListItem1: string = 'Каталог'
-  readonly subTitleListItem2: string = 'Строительные материалы'
-  readonly subTitleListItem3: string = 'Инструменты'
-  readonly subTitleListItem4: string = 'Электрика'
-  readonly subTitleListItem5: string = 'Интерьер и одежда'
+  isShowCatalog: boolean = true;
+  isShowImage: boolean = true;
+  isUpperCase: boolean = true;
+
+  menuItems: string[] = ['Каталог', 'Строительные материалы', 'Инструменты', 'Электрика', 'Интерьер и одежда']
+
+  changeMenuText() {
+    this.menuItems = this.menuItems.map((item: string) => this.isUpperCase ? item.toLowerCase() : item.toUpperCase())
+
+    this.isUpperCase = !this.isUpperCase
+  }
+
+  newPages: number[] = newPages;
+
 
 }
