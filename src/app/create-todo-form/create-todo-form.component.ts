8fae0c692e4f8e6a12c1d5d75ae560071d8a45ba
Sapienti17 <sapienti17@gmail.com>
2025-07-18 15:54:57 +0300

    deleted commit

--- src/app/create-todo-form/create-todo-form.component.ts
+++ src/app/create-todo-form/create-todo-form.component.ts
@@ -1,16 +1,41 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+  ValidatorFn,
+} from '@angular/forms';
 import { ITodo } from '../Interfaces/todo.interface';
+import { MatButtonModule } from '@angular/material/button';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+
+
+function completedValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value?.trim().toLowerCase();
+    if ( value === 'да' || value === 'нет' ) {
+      return null
+    } else {
+      return { invalidCompleted: true }
+    }
+  }
+}
+
 
 @Component({
   selector: 'app-create-todo-form',
   standalone: true,
   imports: [
-    ReactiveFormsModule,
+    ReactiveFormsModule, MatButtonModule, MatLabel, MatFormField, MatError, MatInput,
   ],
   templateUrl: './create-todo-form.component.html',
   styleUrl: './create-todo-form.component.scss',
 })
+
 export class CreateTodoFormComponent {
   @Output()
   public createTodo: EventEmitter<ITodo> = new EventEmitter()
@@ -19,14 +44,19 @@ export class CreateTodoFormComponent {
 
   constructor(private fb: FormBuilder) {
     this.todoForm = this.fb.group({
-      userId: [[], [Validators.required, Validators.minLength(4)]],
-      title: [[], [Validators.required, Validators.minLength(3)]],
-      completed: [[], Validators.required],
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      userId: ['', [Validators.required, Validators.minLength(4)]],
+      completed: ['', [Validators.required, completedValidator()]],
     });
   }
 
+  private getCompletedValue() {
+    const value: string = this.todoForm.get('completed')?.value.trim().toLowerCase();
+    return value === 'да' ? true : false;
+  }
+
   todoSubmit() {
-    this.createTodo.emit(this.todoForm.value)
+    this.createTodo.emit({ ...this.todoForm.value, completed: this.getCompletedValue() })
     this.todoForm.reset()
   }
 }
